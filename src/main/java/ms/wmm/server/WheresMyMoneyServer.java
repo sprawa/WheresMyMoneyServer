3eeccc03bcd9c706106d6dcb05e2a3bdad318ab2
Marcin Sprawka <marcin.sprawka@accenture.com>
2016-05-10 19:49:27 +0200

    Project created

---
+++ src/main/java/ms/wmm/server/WheresMyMoneyServer.java
@@ -0,0 +1,12 @@
+package ms.wmm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheresMyMoneyServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheresMyMoneyServer.class, args);
+	}
+}
---
+++ src/test/java/ms/wmm/server/WheresMyMoneyServerTests.java
@@ -0,0 +1,16 @@
+package ms.wmm.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WheresMyMoneyServer.class)
+public class WheresMyMoneyServerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
